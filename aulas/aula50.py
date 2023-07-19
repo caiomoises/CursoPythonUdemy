bf0d3a5a554c18e310797eae403abd2c3b2983f5
Caio Moisés <caio.cavalcante@alunos.ufersa.edu.br>
2023-07-18 22:22:04 -0300

    up

---
+++ aulas/aula50.py
@@ -0,0 +1,10 @@
+'''
+Exercício
+Exiba os índices da lista
+'''
+
+lista = ['Caio', 'Kézia', 'Marcelo', 'Rivania']
+indices = range(len(lista))
+
+for indice in indices:
+    print(indice, lista[indice], type(lista[indice]))
---
+++ aulas/aula52.py
@@ -0,0 +1,11 @@
+'''
+Tipo tupla - uma lista imutável
+'''
+# Criando uma tupla
+nomes = 'Caio', 'Kézia', 'Marcelo', 'Rivania'
+print(nomes)
+
+# Convertendo lista para tupla
+nomes = ['Caio', 'Kézia', 'Marcelo', 'Rivania']
+nomes = tuple(nomes)
+print(nomes)
